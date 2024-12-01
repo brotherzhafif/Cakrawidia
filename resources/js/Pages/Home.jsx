73349368f4079e4a00d20b97d2384f6c418b5109
Yudhaaaaa <yudhapratama11005@gmail.com>
2024-12-02 00:11:04 +0700

    Nambah komponen Button -H

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Head, Link } from "@inertiajs/react";
 import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
+import Button from "../Components/Button.jsx";
+
 export default function Home() {
     return (
         <>
         <Head title="Home" />
         <div data-theme="light" className="flex flex-col min-h-screen justify-between">
         <Navbar />
-            <div className=" flex flex-col gap-2 grow items-center justify-center">
+            <div className=" flex flex-col gap-2 h-screen items-center justify-center">
                 <h1 className="
                 text-center
                 text-6xl 
@@ -36,12 +38,35 @@ export default function Home() {
                             <option>Lisa</option>
                             <option>Maggie</option>
                         </select>
-                    <button className="btn btn-primary mt-5 font-bold">Ajukan</button>
+                    <Button text="Mulai Bertanya" size="lg" color="secondary" htmlFor="my_modal_6" />
                     </div>
-
                     <div className="modal-action">
                     </div>
                 </div>
+                </div>
+            </div>
+            <div className="flex flex-col w-full h-screen justify-center items-center gap-5">
+                <div className=" items-center flex flex-col bg-slate-500 w-full p-4 h-[50%] gap-5">
+                    <div className="w-full">
+                        <div className="flex flex-row gap-9 mt-[20px] items-center">
+                            <img src="/assets/img/profile_user.jpeg" className="rounded-full w-16"></img>
+                            <ul className="flex flex-row gap-5">
+                                <li className="  h-8 text-sl rounded-[5px]  font-bold flex flex-row gap-2">
+                                    <span>awokawok</span>
+                                    <span>.</span>
+                                    <span>16 Menit Yang lalau</span>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                        <div className="mt-10 justify-center items-center">
+                            <h1 className="font-bold text-5xl">Apa Penyebab Kematian</h1>
+                        </div>
+                        {/* <Button /> */}
+                    </div>
+                </div>
+                <div className="div 2">
+
                 </div>
             </div>
         <Footer />
