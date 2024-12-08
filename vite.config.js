db4b1cc7c273c59bdb651a7364cda1339d7208cc
brotherzhafif <bangz1504@gmail.com>
2024-12-09 00:05:40 +0700

    coba coba

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.jsx'], // Entry point React
-      refresh: true,
-    }),
-    react(),
-  ],
-  build: {
-    outDir: 'public/build',
-    emptyOutDir: true,
-  },
-  base: process.env.APP_URL || '/', // Pastikan URL asset sesuai dengan HTTPS
+    plugins: [
+        react(),
+        laravel({
+            input: [
+                'resources/js/app.jsx', // Jika menggunakan React
+                'resources/css/app.css', // Jika menggunakan CSS atau Tailwind
+            ],
+            refresh: true, // Enable HMR di Laravel saat development
+            build: {
+                outDir: 'public/build',
+                emptyOutDir: true,
+            },
+            base: process.env.APP_URL || '/', // Pastikan URL asset sesuai dengan HTTPS
+        }),
+    ],
 });
