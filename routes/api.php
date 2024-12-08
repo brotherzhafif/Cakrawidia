cd7429b85567fc5c66d9f625df4336bfdfefb924
brotherzhafif <bangz1504@gmail.com>
2024-12-08 19:37:35 +0700

    minor changes

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,12 @@ use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\LikeController;
 
 Route::middleware('api')->group(function () {
-    // users
     Route::apiResource('users', UserController::class);
-    Route::get('/users/{id}', [UsersController::class]);
-
-
-
-    // topics
     Route::apiResource('topics', TopicController::class);
-
-    // questions
     Route::apiResource('questions', QuestionController::class);
     Route::get('/questions/{id}', [QuestionController::class, 'show']);
     Route::apiResource('answers', AnswerController::class);
 
-    // likes
     Route::get('likes', [LikeController::class, 'index']);
     Route::get('likes/{entityType}/{entityId}', [LikeController::class, 'getLikesByEntity']);
     Route::post('likes', [LikeController::class, 'store']);
