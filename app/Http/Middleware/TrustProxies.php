ff133770363b0b0a4503d17d2e299661e1e0c53c
brotherzhafif <bangz1504@gmail.com>
2024-12-08 22:33:18 +0700

    Fix TrustProxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
-class TrustProxies
+class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
@@ -19,5 +19,5 @@ class TrustProxies
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = SymfonyRequest::HEADER_X_FORWARDED_ALL;
 }
