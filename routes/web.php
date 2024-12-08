a4e113a2db62dfd6c28c6cfddf42dc1880bad0e4
9riffegndi <marrief19@gmail.com>
2024-12-08 19:35:24 +0700

    kk

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,14 @@ Route::get('/ProfileUser', function () {
     return Inertia::render('ProfileUser', []);
 })->name('ProfileUser');
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 599aef7ce6bfbd7d63662729b9970d61cfeb5308
 Route::get('/ViewQuestion/{id}', function ($id) {
     return Inertia::render('ViewQuestion', [
         'id' => $id, // Kirim ID ke halaman Inertia
     ]);
 })->name('ViewQuestion');
 
-<<<<<<< HEAD
 
-=======
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -38,4 +32,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
->>>>>>> 599aef7ce6bfbd7d63662729b9970d61cfeb5308
