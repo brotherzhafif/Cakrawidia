4d5d35af7a105d4e0bc1e4e34e9341d1c89ee0ac
brotherzhafif <bangz1504@gmail.com>
2024-12-10 00:04:18 +0700

    Code Cleaning making the repo Only Being API

--- resources/js/partials/Logo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logo() {
-    return (
-        <>
-        <a href="/">
-            <img className="w-16 md:flex hidden " src={"/assets/img/logo.png"} alt="Logo" />
-        </a>
-        </>
-    )
-}
